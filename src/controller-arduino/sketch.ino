79e0e53309cacf80dd7a9d57a59a6a0136775c22
Chris Lieb <chris.lieb@gmail.com>
2025-01-23 21:42:41 -0500

    Simple Arduino controller

--- src/controller-arduino/sketch.ino
+++ src/controller-arduino/sketch.ino
@@ -2,8 +2,9 @@
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
-#define PIN        2
+#define PIN         2
 #define NUMPIXELS 119
+#define MSGSIZE     4
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 #define DELAYVAL 50
@@ -12,17 +13,26 @@ void setup() {
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
 #endif
-
+  Serial.begin(115200);
   pixels.begin();
 }
 
 void loop() {
-  pixels.clear();
-
-  for(int i=0; i<NUMPIXELS; i++) {
-
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-    pixels.show();
-    delay(DELAYVAL);
+  if (Serial.available() >= MSGSIZE) {
+    byte message[MSGSIZE];
+    int bytesReadLen = Serial.readBytes(message, MSGSIZE);
+    if (bytesReadLen == MSGSIZE) {
+      Serial.print("Setting pixel ");
+      Serial.print(message[0]);
+      Serial.print(" to RGB [");
+      Serial.print(message[1]);
+      Serial.print(", ");
+      Serial.print(message[2]);
+      Serial.print(", ");
+      Serial.print(message[3]);
+      Serial.print("]\n");
+      pixels.setPixelColor(message[0], pixels.Color(message[1], message[2], message[3]));
+      pixels.show();
+    }
   }
 }
